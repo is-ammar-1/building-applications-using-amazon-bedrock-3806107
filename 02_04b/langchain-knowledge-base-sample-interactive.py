9dbd06f05bee29677a8432401944cbd8e7d9e37c
Muhammad Ammar <ammarshafique677@gmail.com>
2024-10-18 07:42:31 +0000

    modified:   02_04b/langchain-knowledge-base-sample-interactive.py modified:   .vscode/settings.json modified:   02_04b/langchain-knowledge-base-sample-interactive.py

--- 02_04b/langchain-knowledge-base-sample-interactive.py
+++ 02_04b/langchain-knowledge-base-sample-interactive.py
@@ -1,11 +1,34 @@
 #Imports
+from langchain.chains import RetrievalQA
+from langchain_community.llms import bedrock
+from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 
 #Define the retriever
+retriever = AmazonKnowledgeBasesRetriever(
+  knowledge_base_id="DAVTPNHPOP",
+  retrieval_config={"vectorSearchConfiguration":{"numberOfResults":4}}
+)
 
 #Define model parameters
+model_kwargs_meta = {
+  "temperature":0,
+  "top_k":10,
+  "max_tokens_to_sample":750,
 
+}
 #Configure llm
-
+llm = bedrock(
+  model_id = "meta.llama3-1-405b-instruct-v1:0",
+  model_kwargs=model_kwargs_meta
+)
 #Configure the chain
-
+qa = RetrievalQA.from_chain_type(
+llm = llm,
+retriever = retriever,
+return_source_documents = True
+)
 #Get user input and display the result
+while True:
+  query = input("\n Ask me a question: \n")
+  output = qa.invoke(query)
+  print(output['result'])
